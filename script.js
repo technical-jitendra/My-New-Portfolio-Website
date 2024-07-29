fed755a66f7cebb822e8033809bf05ec8c83ee68
technical-jitendra <jitendrakumarjjk10@gmail.com>
2024-07-29 14:50:22 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello my name is jitendra kumar")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGO');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGO')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.ham').style.display='none'
+        setTimeout(() => {
+            document.querySelector('.cross').style.display='inline' 
+        }, 300);
+        
+    }
+})
